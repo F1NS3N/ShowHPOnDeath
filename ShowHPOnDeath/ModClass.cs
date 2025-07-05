3e7358962f17529381ff45b3ad4ab2fb551185a5
FIN <kyzmindimon1311@gmail.com>
2025-07-05 04:46:59 +0300

    Game Name Update

--- ShowHPOnDeath/ModClass.cs
+++ ShowHPOnDeath/ModClass.cs
@@ -33,7 +33,6 @@ namespace ShowHPOnDeath
 
         private string BeforeSceneLoad(string newSceneName)
         {
-            Log($"[SCENE] Подготовка к загрузке новой сцены: {newSceneName}");
 
             UpdateBossRemnantHP();
 
@@ -62,11 +61,11 @@ namespace ShowHPOnDeath
 
         private void OnHealthManagerEnable(On.HealthManager.orig_OnEnable orig, HealthManager self)
         {
-            if (Bosses.Contains(self.gameObject.name))
+            if (BossNames.TryGetValue(self.gameObject.name, out string displayName))
             {
-                Log($"[ShowHPOnDeath] Обнаружен босс: {self.gameObject.name}");
+                Log($"[ShowHPOnDeath] Обнаружен босс: {self.gameObject.name} ({displayName})");
                 _currentBoss = self;
-                LastBossName = self.gameObject.name;
+                LastBossName = displayName;
             }
             orig(self);
         }
@@ -93,54 +92,66 @@ namespace ShowHPOnDeath
             }
         }
 
-        public static List<string> Bosses = new List<string>()
+        public static Dictionary<string, string> BossNames = new Dictionary<string, string>()
         {
             // Hall Of Gods
-            "Giant Fly", // Gruz Mother
-            "Giant Buzzer Col", "Giant Buzzer Col (1)", // Vengerfly King
-            "Mawlek Body", // Brooding Mawlek
-            "False Knight New", // False Knight
-            "False Knight Dream", // Failed Champion
-            "Hornet Boss 1", // Hornet Protector (1)
-            "Hornet Boss 2", // Hornet Sentinel (2)
-            "Mega Moss Charger", // Massive Moss Charger
-            "Fluke Mother", // Flukemarm
-            "Mantis Lord", // Mantis Lord Phase 1
-            "Mantis Lord S1", "Mantis Lord S2", // Mantis Lord Phase 2
-            "Mantis Lord S3", // Sisters Of Battle
-            "Mega Fat Bee", "Mega Fat Bee (1)", // Oblobbles
-            "Hive Knight", // Hive Knight
-            "Infected Knight", // Broken Vessel
-            "Lost Kin", // Lost Kin
-            "Mimic Spider", // Nosk
-            "Hornet Nosk", // Winged Nosk
-            "Jar Collector", // The Collector
-            "Lancer", "Lobster", // God Tamer
-            "Mega Zombie Beam Miner (1)", // Crystal Guardian
-            "Zombie Beam Miner Rematch", // Enraged Guardian
-            "Mega Jellyfish GG", // Uumu
-            "Mantis Traitor Lord", // Traitor Lord
-            "Grey Prince", // GPZ
-            "Mage Knight", // Soul Warrior
-            "Mage Lord", "Mage Lord Phase2", // Soul Master
-            "Dream Mage Lord", "Dream Mage Lord Phase2", // Soul Tyrant
-            "Dung Defender", // Dung Defender
-            "White Defender", // White Defender
-            "Black Knight 1", "Black Knight 2", "Black Knight 3", "Black Knight 4", "Black Knight 5", "Black Knight 6", // Watchers Knight
-            "Ghost Warrior No Eyes", // No Eyes
-            "Ghost Warrior Marmu", // Marmu
-            "Ghost Warrior Xero", // Xero
-            "Ghost Warrior Markoth", // Markoth
-            "Ghost Warrior Galien", // Galien
-            "Ghost Warrior Slug", // Gorb
-            "Ghost Warrior Hu", // Elder Hu
-            "Mato", "Oro", // Oro & Mato
-            "Sheo Boss", // Paintmaster Sheo
-            "Sly Boss", // Nailsage Sly
-            "HK Prime", // Pure Vessel
-            "Grimm Boss", // Grimm
-            "Nightmare Grimm Boss", // Nightmare King
-            "Absolute Radiance", // Radiance
+            { "Giant Fly", "Gruz Mother" },
+            { "Giant Buzzer Col", "Vengerfly King" },
+            { "Giant Buzzer Col (1)", "Vengerfly King" }, // дубликаты тоже можно обрабатывать
+            { "Mawlek Body", "Brooding Mawlek" },
+            { "False Knight New", "False Knight" },
+            { "False Knight Dream", "Failed Champion" },
+            { "Hornet Boss 1", "Hornet Protector" },
+            { "Hornet Boss 2", "Hornet Sentinel" },
+            { "Mega Moss Charger", "Massive Moss Charger" },
+            { "Fluke Mother", "Flukemarm" },
+            { "Mantis Lord", "Mantis Lord Phase 1" },
+            { "Mantis Lord S1", "Mantis Lord Phase 2" },
+            { "Mantis Lord S2", "Mantis Lord Phase 2" },
+            { "Mantis Lord S3", "Sisters Of Battle" },
+            { "Mega Fat Bee", "Oblobbles" },
+            { "Mega Fat Bee (1)", "Oblobbles" },
+            { "Hive Knight", "Hive Knight" },
+            { "Infected Knight", "Broken Vessel" },
+            { "Lost Kin", "Lost Kin" },
+            { "Mimic Spider", "Nosk" },
+            { "Hornet Nosk", "Winged Nosk" },
+            { "Jar Collector", "The Collector" },
+            { "Lancer", "God Tamer" },
+            { "Lobster", "God Tamer" },
+            { "Mega Zombie Beam Miner (1)", "Crystal Guardian" },
+            { "Zombie Beam Miner Rematch", "Enraged Guardian" },
+            { "Mega Jellyfish GG", "Uumu" },
+            { "Mantis Traitor Lord", "Traitor Lord" },
+            { "Grey Prince", "Grey Prince Zenker" },
+            { "Mage Knight", "Soul Warrior" },
+            { "Mage Lord", "Soul Master" },
+            { "Mage Lord Phase2", "Soul Master" },
+            { "Dream Mage Lord", "Soul Tyrant" },
+            { "Dream Mage Lord Phase2", "Soul Tyrant" },
+            { "Dung Defender", "Dung Defender" },
+            { "White Defender", "White Defender" },
+            { "Black Knight 1", "Watcher Knight" },
+            { "Black Knight 2", "Watcher Knight" },
+            { "Black Knight 3", "Watcher Knight" },
+            { "Black Knight 4", "Watcher Knight" },
+            { "Black Knight 5", "Watcher Knight" },
+            { "Black Knight 6", "Watcher Knight" },
+            { "Ghost Warrior No Eyes", "No Eyes" },
+            { "Ghost Warrior Marmu", "Marmu" },
+            { "Ghost Warrior Xero", "Xero" },
+            { "Ghost Warrior Markoth", "Markoth" },
+            { "Ghost Warrior Galien", "Galien" },
+            { "Ghost Warrior Slug", "Gorb" },
+            { "Ghost Warrior Hu", "Elder Hu" },
+            { "Mato", "Oro & Mato" },
+            { "Oro", "Oro & Mato" },
+            { "Sheo Boss", "Paintmaster Sheo" },
+            { "Sly Boss", "Nailsage Sly" },
+            { "HK Prime", "Pure Vessel" },
+            { "Grimm Boss", "Grimm" },
+            { "Nightmare Grimm Boss", "Nightmare King" },
+            { "Absolute Radiance", "Radiance" },
         };
     }
 }
